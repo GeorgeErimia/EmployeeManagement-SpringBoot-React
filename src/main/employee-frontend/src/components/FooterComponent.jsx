29734c91108dd3a4230201542b1002051dcda251
George Erimia <l3ttuceftw@gmil.com>
2024-03-12 20:17:02 +0200

    Created Update button for each employee (not fully working yet) Created Routing for the update button and separate page for it

---
+++ src/main/employee-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer className="footer">
+        Employee Management System By George Erimia.
+      </footer>
+    </div>
+  );
+};
+
+export default FooterComponent;
